d8d7412b2a101e3c1e3ec7534941310b34bbc945
haithemzeggari <gh_zeggari@esi.dz>
2019-11-24 11:21:10 +0100

    commit module 4

--- Telly/src/com/telly/service/BusService.java
+++ Telly/src/com/telly/service/BusService.java
@@ -16,9 +16,7 @@ public class BusService {
 	@Autowired
 	private BusDao busDao;
 	
-	public void create(Bus bus){
-		busDao.create(bus);
-	}
+	
 	
 	public List<Bus> getCity(String leaveFrom, String goingTo, Date dateLeave, Date dateReturn){
 		return busDao.getCity(leaveFrom, goingTo, dateLeave, dateReturn);
